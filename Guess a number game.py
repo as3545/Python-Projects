c0525a2c1d938e2f63c5e8a5de808f3997ae49be
as3545 <86479682+as3545@users.noreply.github.com>
2024-08-12 14:06:23 +0530

    Guess a number game.py

---
+++ Guess a number game.py
@@ -0,0 +1,26 @@
+#Game code
+import random
+import math
+l=int(input("Enter lower range "))
+h=int(input("Enter higher range "))
+r=random.randint(l, h)
+chance=math.ceil(math.log(h-l+1,2))
+print(chance)
+c=0
+flag=False
+while c<chance:
+    c+=1
+    guess=int(input("Guess a number "))
+    if r==guess:
+        print("Congratulation ")
+        print("Game Over")
+        flag=True
+        break
+    elif r<guess:
+        print("Too high ")
+    else:
+        print("Too low ")
+if not flag:
+    print("Try again")
+    print("The result is %d" % r)
+    print("Game Over")
